b0bf9f5d8bbaae6bdacd44d6ff8d7c9fbcc29c4b
Decio Soares <decio.msoares@gmail.com>
2023-12-28 00:03:07 +0000

    Tweak picom opacity rules

--- home/services/picom/default.nix
+++ home/services/picom/default.nix
@@ -4,10 +4,14 @@
     activeOpacity = 1.0;
     inactiveOpacity = 0.8;
     backend = "glx";
+    vSync = true;
     fade = true;
     fadeDelta = 2;
     shadow = true;
     shadowOpacity = 0.75;
+    opacityRules = [
+      "100:class_g   *?= 'Rofi'"
+    ];
     # solving google chrome weird shadows around menus
     # https://github.com/orgs/regolith-linux/discussions/949
     wintypes = {
