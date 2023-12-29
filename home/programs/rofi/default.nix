a8191b2b90af10ddc406805b2a64e634dec30948
Decio Soares <decio.msoares@gmail.com>
2023-12-29 02:41:01 +0000

    Fix rofi keybindings

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -6,6 +6,12 @@
     plugins = with pkgs; [ rofi-calc rofi-emoji ];
     terminal = "${pkgs.alacritty}/bin/alacritty";
     theme = "gruvbox-dark-hard";
+    extraConfig = {
+      kb-remove-to-eol = "";
+      kb-accept-entry = "Control+m,Return,KP_Enter";
+      kb-row-up = "Up,Control+k";
+      kb-row-down = "Down,Control+j";
+    };
   };
 
   # for rofi-emoji to insert emojis directly
