ad738a8febb8e33d717773077d78c0718b7a4b99
Decio Soares <decio.msoares@gmail.com>
2023-12-30 16:10:34 +0000

    Reorganize xmobar and trayer

--- home/services/trayer/default.nix
+++ home/services/trayer/default.nix
@@ -7,10 +7,14 @@
       align = "right";
       padding = 6;
       SetDockType = true;
+      SetPartialStrut = true;
+      expand = true;
       widthtype = "request";
       height = 22;
       distancefrom = "right";
-      distance = 10;
+      distance = 180;
+      transparent = true;
+      alpha = 0;
       tint = "0x28282800";
     };
   };
